eb6cb2060df45660802357963c412b99818a6c56
HynekSvoboda <97514770+HynekSvoboda@users.noreply.github.com>
2023-06-07 09:03:23 +0200

    😂

---
+++ Test-Vyjimky/Test-Vyjimky/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Test_Vyjimky
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 331);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "-9\r\nz\r\nfgsdfg\r\n1546\r\ndsfgdsf\r\n-999999999999\r\nsdfgdsg\r\n5\r\n-8\r\n";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(146, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 83);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Spustit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(272, 351);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Test-Vyjimky/Test-Vyjimky/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Vyjimky
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = textBox1.Lines.Count();
+            int[] pole = new int[n];
+            int pomocna = 0;
+            int soucet = 0;
+            for(int i=0;i<n;i++)
+            {
+                try
+                {
+                    int cislo = Convert.ToInt32(textBox1.Lines[i]);
+                    if (cislo < 0)
+                    {
+                        pole[pomocna] = 0;
+                        checked { soucet += cislo; }
+                        pomocna++;
+                    }
+                }
+                catch(FormatException) { }
+                catch (OverflowException) { }
+                catch (ArithmeticException) { }
+            }
+            try
+            {
+               checked
+                {
+                    double prumer = (double)soucet /(double)pomocna;
+                    if(prumer<0) MessageBox.Show(prumer.ToString(),"Průměr celých záporných čísel ",MessageBoxButtons.OK);
+                    else MessageBox.Show(prumer.ToString(), "Žádná záporná čísla nenalezena ", MessageBoxButtons.OK);
+                }
+            }
+            catch (DivideByZeroException) { }
+        }
+    }
+}
